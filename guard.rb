fc8d23d84bd3f93326f104a35664cab808dbb09c
Fletcher Nichol <fnichol@nichol.ca>
2011-03-01 21:20:53 -0700

    Add livereload to Guardfile.

--- guard.rb
+++ guard.rb
@@ -75,6 +75,13 @@ create_file 'Guardfile' do
       watch(%r{features/support/.+})          { 'features' }
       watch(%r{features/step_definitions/.+}) { 'features' }
     end
+
+    guard 'livereload' do
+      watch(%r{app/.+\.(erb|haml)})
+      watch(%r{app/helpers/.+\.rb})
+      watch(%r{public/.+\.(css|js|html)})
+      watch(%r{config/locales/.+\.yml})
+    end
   GUARDFILE
 end
 
